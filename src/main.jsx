932cde64a863aead9686e4e0b552a3a8fefba33f
Marte <125874444+mar-ols@users.noreply.github.com>
2024-10-17 18:44:29 +0200

    Installs and adds Bootstrap to app

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { Theme } from "./styles/theme.jsx";
 import GlobalStyle from "./styles/GlobalStyle.jsx";
 import App from "./App.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/index.css";
 
 createRoot(document.getElementById("root")).render(
