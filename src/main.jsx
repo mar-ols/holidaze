392397000ca446f376f5f765c7a38c7cc0730b5a
Marte <125874444+mar-ols@users.noreply.github.com>
2024-10-17 18:47:16 +0200

    Installs and adds Bootstrap JS

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { Theme } from "./styles/theme.jsx";
 import GlobalStyle from "./styles/GlobalStyle.jsx";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./styles/index.css";
 
 createRoot(document.getElementById("root")).render(
