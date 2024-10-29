b9354f4abc14f2d4040d7f13c880678f6334831f
Marte <125874444+mar-ols@users.noreply.github.com>
2024-10-29 15:20:46 +0100

    Removes fetched venues

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,8 @@ import { Venue } from "./pages/venues";
 import { Profile } from "./pages/profile";
 import { About } from "./pages/about";
 import "./styles/App.css";
-// import { useFetch } from "./components/api/constant";
 
 function App() {
-  // const {
-  //   data: venues,
-  //   isLoading,
-  //   isError,
-  // } = useFetch("https://v2.api.noroff.dev/holidaze/venues");
-
-  // if (isLoading) return <div>Loading venues...</div>;
-  // if (isError) return <div>Error: {isError}</div>;
-
-  // console.log(venues);
-
   return (
     <>
       <Routes>
@@ -32,16 +20,6 @@ function App() {
           <Route path="*" element={<div>Route not found</div>} />
         </Route>
       </Routes>
-      {/* <h1>Vite + React</h1>
-      {venues && venues.data ? (
-        <ul>
-          {venues.data.map((venue) => (
-            <li key={venue.id}>{venue.name}</li>
-          ))}
-        </ul>
-      ) : (
-        <p>No venues available</p>
-      )} */}
     </>
   );
 }
